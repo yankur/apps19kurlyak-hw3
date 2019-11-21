87fcf6b87350a8f351cd5cd08f393471895b0f2c
yankur <42523950+yankur@users.noreply.github.com>
2019-11-21 09:40:39 +0200

    Update SortDecorator.java

--- src/main/java/ua/edu/ucu/smartarr/SortDecorator.java
+++ src/main/java/ua/edu/ucu/smartarr/SortDecorator.java
@@ -1,6 +1,36 @@
 package ua.edu.ucu.smartarr;
+import ua.edu.ucu.functions.MyComparator;
+import java.util.Arrays;
 
 // Sorts elements using MyComparator to compare them
-public class SortDecorator {
-    
+public class SortDecorator extends SmartArrayDecorator{
+    private MyComparator comparator;
+
+    public SortDecorator(SmartArray smartArray, MyComparator cmp) {
+        super(smartArray);
+        this.comparator = cmp;
+        sort();
+    }
+
+    private void sort() {
+        Object[] newArr = new Object[size()];
+        System.arraycopy(toArray(), 0, newArr, 0, size());
+        Arrays.sort( newArr, comparator);
+        smartArray = new BaseArray(newArr);
+    }
+
+    @Override
+    public Object[] toArray() {
+        return smartArray.toArray();
+    }
+
+    @Override
+    public String operationDescription() {
+        return "Sorts elements using MyComparator to compare them";
+    }
+
+    @Override
+    public int size() {
+        return smartArray.size();
+    }
 }
