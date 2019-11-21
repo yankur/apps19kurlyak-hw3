3925ffb9f947bf9564bcd2291fcbd8160f85b6a7
yankur <42523950+yankur@users.noreply.github.com>
2019-11-21 09:39:57 +0200

    Update MapDecorator.java

--- src/main/java/ua/edu/ucu/smartarr/MapDecorator.java
+++ src/main/java/ua/edu/ucu/smartarr/MapDecorator.java
@@ -1,6 +1,39 @@
 package ua.edu.ucu.smartarr;
+import ua.edu.ucu.functions.MyFunction;
+import java.util.Arrays;
 
 // Map every element to another object using MyFunction
-public class MapDecorator {
+public class MapDecorator extends SmartArrayDecorator{
+    private MyFunction function;
 
+    public MapDecorator(SmartArray smartArray, MyFunction func) {
+        super(smartArray);
+        this.function = func;
+        map();
+    }
+
+    private void map() {
+        Object[] baseArr = smartArray.toArray();
+        Object[] newArr = new Object[size()];
+        for (int i = 0; i < size(); i++) {
+            newArr[i] = function.apply(baseArr[i]);
+        }
+        this.smartArray = new BaseArray(Arrays.copyOf(newArr, size()));
+    }
+
+    @Override
+    public Object[] toArray() {
+        return smartArray.toArray();
+    }
+
+    @Override
+    public String operationDescription() {
+        return "Map every element to another object using MyFunction";
+    }
+
+    @Override
+    public int size() {
+        return smartArray.size();
+    }
 }
+
