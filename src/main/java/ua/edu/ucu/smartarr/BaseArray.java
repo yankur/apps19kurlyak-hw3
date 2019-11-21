c4817a80e4f1d99cdbdb07c3c92483c7ee958bcd
yankur <42523950+yankur@users.noreply.github.com>
2019-11-21 09:38:37 +0200

    Update BaseArray.java

--- src/main/java/ua/edu/ucu/smartarr/BaseArray.java
+++ src/main/java/ua/edu/ucu/smartarr/BaseArray.java
@@ -1,6 +1,32 @@
 package ua.edu.ucu.smartarr;
 
-// Base array for decorators
-public class BaseArray {
+import java.util.Arrays;
 
+// Base array for decorators
+public class BaseArray implements SmartArray{
+    private Object[] arr;
+    private int size;
+
+    public BaseArray(Object[] newArr) {
+        if (newArr.length == 0) {
+            throw new IllegalArgumentException();
+        }
+        arr = Arrays.copyOf(newArr, newArr.length);
+        size = arr.length;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return Arrays.copyOf(arr, arr.length);
+    }
+
+    @Override
+    public String operationDescription() {
+        return "Base array for decorators";
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
 }
